cdaebb6468f370fbced43292fe37cf5de67819a2
Eduardo Miguel <el.chiry66@gmail.com>
2020-03-14 06:12:33 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
 const connectDB = async () => {
+  const URI= 'mongodb+srv://root:Cub1t4n40w@cluster0-mxgxf.mongodb.net/MERN-Tasks'
     try {
-        await mongoose.connect(process.env.DB_MONGO, {
+        await mongoose.connect(URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
