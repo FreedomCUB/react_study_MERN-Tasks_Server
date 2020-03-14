a6f256c50e40b4c7bb54973f54aeb05966c0bf27
Eduardo Miguel <el.chiry66@gmail.com>
2020-03-14 06:02:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // app port
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Import routes
 app.use("/api/users", require("./routes/users"));
