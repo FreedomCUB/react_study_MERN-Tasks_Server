c589a23caf748b4696011b84338282e37ac0e18b
Eduardo Miguel <el.chiry66@gmail.com>
2020-03-14 05:41:08 -0300

    FINAL

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db')
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 // create server
 const app = express();
@@ -12,20 +12,18 @@ connectDB();
 app.use(cors());
 
 // expres.json
-app.use( express.json({ extended: true}) );
-
-// Impor routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/projects', require('./routes/projects'))
-app.use('/api/tasks', require('./routes/tasks'))
-
-
+app.use(express.json({ extended: true }));
 
 // app port
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
+
+// Import routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/projects", require("./routes/projects"));
+app.use("/api/tasks", require("./routes/tasks"));
 
 // app start
-app.listen(port, '0.0.0.0' , () => {
-    console.log(`App funcionando en el puerto ${port}`);
-})
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server funcionando en el puerto ${port}`);
+});
